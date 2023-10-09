7f05dabaf8881902d64db34be040241b42363f80
t-dlsantos <thiagodlsantos9@gmail.com>
2023-10-09 13:20:25 -0300

    feat: add interaction with the html and logic for the result

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let displayValue = '';
+
+const currentOperation = document.querySelector('.current-operation');
+const lastOperation = document.querySelector('.last-operation');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equals = document.querySelector('.equals');
 
 function add(a, b) {
     return a + b;
@@ -15,24 +22,56 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return 'Error: Division by zero';
+    } 
     return a / b;
 }
 
-function operate() {
-    const firstNumber = Number(prompt('a: ', 0));
-    const secondNumber = Number(prompt('b: ', 0));
-    const operator = prompt('operator: ', '+');
-    
+function operate(firstNumber, secondNumber, operator) {
+    console.log(firstNumber, secondNumber, operator);
     switch(operator) {
         case '+':
-            add(firstNumber, secondNumber);
+            return add(firstNumber, secondNumber);
         case '-':
-            subtract(firstNumber, secondNumber);
-        case '*':
-            multiply(firstNumber, secondNumber);
-        case '/':
-            divide(firstNumber, secondNumber);
+            return subtract(firstNumber, secondNumber);
+        case 'x':
+            return multiply(firstNumber, secondNumber);
+        case '÷':
+            return divide(firstNumber, secondNumber);
+        default:
+            return 'Invalid Operator';
     }
 }
 
-// operate();
+function updateDisplay(e) {
+    const dataNumber = e.target.getAttribute('data-number');
+    displayValue += dataNumber;
+    currentOperation.textContent = displayValue;
+}
+
+function getOperator(e) {
+    if(firstNumber === '') {
+        firstNumber = displayValue;
+        displayValue = '';
+    }
+    operator = e.target.getAttribute('data-operator');
+    lastOperation.textContent = `${firstNumber} ${operator}`;
+}
+
+function evaluateEquation() {
+    if(operator && secondNumber === '') {
+        secondNumber = displayValue;
+        const result = operate(Number(firstNumber), Number(secondNumber), operator);
+        currentOperation.textContent = result;
+        firstNumber = result;
+        secondNumber = '';
+        operator = '';
+        lastOperation.textContent = '';
+        displayValue = '';
+    }
+}
+
+numberButtons.forEach(button => button.addEventListener('click', updateDisplay));
+operatorButtons.forEach(operator => operator.addEventListener('click', getOperator));
+equals.addEventListener('click', evaluateEquation);
